64b85b54c4e1b7ba65b56751c82f6c09defd9717
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2021-09-03 19:39:52 +0300

    Mano pirmas bandymas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Sarasas = new List<string>();
+
+            for (int i = 0; i < Sarasas.Count; i++)
+            {
+                Console.WriteLine(Sarasas[i]);
+            }
+
+        }
+    }
+}
